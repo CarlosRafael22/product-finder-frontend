f3a6044d34897756f8ec3d86d608f6a417ca1d45
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2020-11-17 18:13:50 -0300

    refactor(App): Adapt App.js to have Router

--- src/App.js
+++ src/App.js
@@ -1,69 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ProductCard from './components/ProductCard';
-import Navbar from './components/Navbar/Navbar';
+import Products from './pages/Products';
+import Signup from './pages/signup/Signup';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+const Home = () => (<h2>Home</h2>);
+
+const Users = () => (<h2>Users</h2>);
 
 function App() {
-  const [count, setCount] = useState(1);
-  const [productsCounter, setProductsCounter] = useState(0);
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    fetchProducts();
-  }, [count])
-
-  const fetchProducts = async () => {
-    try {
-      const response = await fetch('http://localhost:8000/finder/api/products');
-      const data = await response.json();
-      console.log('Pegou produtos');
-      setProductsCounter(productsCounter + 1);
-      setProducts(data['products']);
-      setCount(data['products'].length);
-      console.log(data['products'][0])
-      console.log(products[0]);
-    } catch (error) {
-      console.log('Deu merda');
-      console.log(error);
-    }
-  }
-
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Magazon Finder
-        </a>
-      </header> */}
-      <Navbar />
-      <h1>Teste do MAGAZON</h1>
-        <h3>Retornou um total de {count} items</h3>
-        <h3>Fez {productsCounter} requisicoes</h3>
-        <button onClick={() => setCount(count + 1)}>
-          Click me
-        </button>
-        <div class="row">
-          {
-            products.map(product => (
-              <ProductCard
-                link={product.link}
-                image_url={product.image_url}
-                name={product.name}
-                price={product.price} />
-            ))
-          }
-        </div>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/products">
+            <Products />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
