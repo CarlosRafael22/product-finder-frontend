ab9159a3e3da9fb49db15d94f1ae4fc6f18db66a
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2020-11-18 14:10:09 -0300

    feat(Navbar): Access state to get loggedIn user

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { useSelector } from 'react-redux';
 import "./Navbar.css";
 
 export default (props) => {
+    const {user, loggedIn} = useSelector(state => state.auth);
+    console.log('NO HEADER: ', user, loggedIn)
 
     return (
         <nav className="navbar">
@@ -9,7 +12,7 @@ export default (props) => {
             <ul className="navbar-items">
                 <li className="nav-links">Search</li>
                 <li className="nav-links">Products</li>
-                <li className="nav-links">Profile</li>
+                <li className="nav-links">{loggedIn ? user.name : 'Log in'}</li>
             </ul>
         </nav>
     )
