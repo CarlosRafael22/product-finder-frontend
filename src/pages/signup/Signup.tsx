c73e65cec482693a9df7ee165d86f703c7bf2bad
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2020-12-18 12:52:36 -0300

    refactor(signup): Refactor to use new Form component

--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -4,12 +4,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import { saveUser } from '../../redux/auth/actions';
 import { RootState } from '../../redux/index';
 import './Signup.css'
-
+import Form, { initialValuesType } from '../../components/Form';
+import InputField from '../../components/Form/InputField';
 
 export const SignupForm = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
     const history = useHistory();
 
     const dispatch = useDispatch();
@@ -19,39 +17,54 @@ export const SignupForm = () => {
         event.preventDefault();
         console.log(event.target);
         const userToCreate = {
-            name,
-            email,
-            password,
-            username: email
+            // name,
+            // email,
+            // password,
+            // username: email,
+            userId: 1,
+            customerId: 1
         };
         console.log(userToCreate)
         dispatch(saveUser(userToCreate));
         // history.push('/products');
     };
 
-    console.log(name)
-    console.log(authUser)
+    const onSubmitHandler = (values: initialValuesType) => {
+        console.log('CHAMOU PAI')
+        console.log(values)
+    };
+
     return (
-        <form className="main-form" onSubmit={submitSignup}>
-            <div className="form-group">
-                <label htmlFor="exampleInputname1">Name</label>
-                <input type="name" className="form-control" id="exampleInputname1" aria-describedby="nameHelp" placeholder="Enter name" value={name} onChange={(event) => setName(event.target.value)} />
-            </div>
-            <div className="form-group">
-                <label htmlFor="exampleInputEmail1">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value={email} onChange={(event) => setEmail(event.target.value)} />
-                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-            </div>
-            <div className="form-group">
-                <label htmlFor="exampleInputPassword1">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" value={password} onChange={(event) => setPassword(event.target.value)} />
-            </div>
-            <div className="form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
+        <Form
+            initialValues={{
+                firstName: '',
+                lastName: '',
+                email: '',
+                password: ''
+            }}
+            onSubmitHandler={onSubmitHandler}
+        >
+            <InputField
+                type='text'
+                name='firstName'
+                placeholder='Enter first name'
+                />
+            <InputField
+                type='text'
+                name='lastName'
+                placeholder='Enter last name'
+                />
+            <InputField
+                type='email'
+                name='email'
+                placeholder='Enter email'
+                />
+            <InputField
+                type='password'
+                name='password'
+                placeholder='Enter password'
+                />
+        </Form>
     )
 };
 
