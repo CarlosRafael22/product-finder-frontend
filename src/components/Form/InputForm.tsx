9e4fe580a3e7a2159bf8e62685cfc91e3c59093d
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2020-12-18 12:48:26 -0300

    feat(inputform): Add name props and change other props default values

--- src/components/Form/InputForm.tsx
+++ src/components/Form/InputForm.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 type InputFormProps = {
     type: string,
+    name: string,
     label: string,
     placeholder: string,
     helpText?: string,
@@ -9,12 +10,12 @@ type InputFormProps = {
     onChangeCallback: Function
 };
 
-const InputForm = ({type, label, placeholder, helpText, value, onChangeCallback}: InputFormProps) => (
+const InputForm = ({type, name, label, placeholder, helpText, value, onChangeCallback}: InputFormProps) => (
     <div className="form-group">
-        <label htmlFor={`${label}Input`}>{label}</label>
-        <input type={type} className="form-control" id={`${label}Input`} aria-describedby={`${label}Help`} aria-label={`${label}Input`}
+        <label htmlFor={`${name}-input`}>{label}</label>
+        <input type={type} className="form-control" id={name} aria-describedby={`${name}-help`} aria-label={name}
             placeholder={placeholder} value={value} onChange={(event) => onChangeCallback(event.target.value)} />
-        {helpText && <small id={`${label}Help`} className="form-text text-muted" data-testid='helpText'>{helpText}</small>}
+        {helpText && <small id={`${name}-help`} className="form-text text-muted" data-testid='helpText'>{helpText}</small>}
     </div>
 );
 
